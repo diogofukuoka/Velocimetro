0eb5e083183375256ce5372f531263e511822461
diogofukuoka <diogofukuoka85@gmail.com>
2026-05-21 13:48:37 -0300

    build: update base path for GitHub Pages deployment
    Set base URL to /Velocimetro/ to ensure correct asset resolution when hosting on GitHub Pages.

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/Velocimetro/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
